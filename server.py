4ff5dded0f01e0843491f4eb1fdbfeffa6013967
aaron3238 <aprit540@live.kutztown.edu>
2020-11-22 16:02:07 -0500

    added regex checking on nickname input, added main to a def, made global for MAXBUFFERSIZE

--- server.py
+++ server.py
@@ -1,69 +1,54 @@
-# #
-# Python program to implement server side of chat room. 
+#!/usr/bin/python
+# Contributors: Aaron Pritchard, David Nelson, Nick Morello, Nik Golombek
+# Filename: server.py
+# Class: CSC328 
+# Prof: Dr. Frye 
+# Major: CS
+# Date: 22 November 2020
+# Assignment: Chat Server
+# Due: 24 November 2020
+# Execute: python server.py
+# Python Version: 2.7.5
+# Purpose: Establishes a connection to a server with either TCP or UDP, which are
+#          collected via command line arguments, and prints out a QOTD message from 
+#          the specified server.
 import socket 
 import select 
 import sys 
 from thread import start_new_thread
+import re
 
-"""The first argument AF_INET is the address domain of the 
-socket. This is used when we have an Internet Domain with 
-any two hosts The second argument is the type of socket. 
-SOCK_STREAM means that data or characters are read in 
-a continuous flow."""
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-
-# checks whether sufficient arguments have been provided 
-if (len(sys.argv) != 2) and (len(sys.argv) != 3): 
-	print "Correct usage: script, IP address, (optional) port number"
-	exit() 
-
-IP_address = str(sys.argv[1])
-
-# takes second argument from command prompt as port number
-if len(sys.argv) == 3:
-	Port = int(sys.argv[2]) 
-else:
-  #default port
-  Port = 8888
-
-#Binds the server to an entered IP address and at the 
-#specified port number. 
-try:
-	server.bind((IP_address, Port)) 
-except socket.error as err:
-	print err
-	exit()
-server.listen(100) 
-list_of_clients = [] 
-clientNicknames = []
+global MAXBUFFERSIZE
+MAXBUFFERSIZE = 2048
 
 def clientthread(conn, addr, clientNicknames): 
 
 	# sends a message to the client whose user object is conn 
-	conn.send("Users currently connected: ")
+	conn.send("\nUsers currently connected: ")
 	conn.send(str(clientNicknames))
 	conn.send("\nEnter a unique nickname:")
 	while True:
 		uniqueName = True
 		nickname = ""		
-		message = conn.recv(2048)
+		message = conn.recv(MAXBUFFERSIZE) 
 		nickname = message.rstrip()
-		for name in clientNicknames:
-			if name == nickname:
-				uniqueName = False
-		if uniqueName:
-			conn.send("READY")
-			clientNicknames.append(nickname)
-			print nickname + " connected"
-			break
+
+		if re.match("^[a-zA-Z0-9]{2,30}$", nickname):
+
+			for name in clientNicknames:
+				if name == nickname:
+					uniqueName = False
+			if uniqueName:
+				conn.send("READY")
+				clientNicknames.append(nickname)
+				print nickname + " connected"
+				break
+			else:
+				print(clientNicknames)
+				conn.send("RETRY")
 		else:
-			print(clientNicknames)
+			conn.send("Nickname must be alphanumberic and 2-30 Characters\n")
 			conn.send("RETRY")
-	
-
-
-
 	
 	while True: 
 			try: 
@@ -105,44 +90,87 @@ def remove(connection):
 	if connection in list_of_clients: # check if the connection is in the list of clients 
 		list_of_clients.remove(connection) # remote it 
 
+# Main Thread
+def main():
+	"""The first argument AF_INET is the address domain of the 
+	socket. This is used when we have an Internet Domain with 
+	any two hosts The second argument is the type of socket. 
+	SOCK_STREAM means that data or characters are read in 
+	a continuous flow."""
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+	# checks whether sufficient arguments have been provided 
+	if (len(sys.argv) != 2) and (len(sys.argv) != 3): 
+		print "Correct usage: script, IP address, (optional) port number"
+		exit() 
+
+	IP_address = str(sys.argv[1])
+
+	# takes second argument from command prompt as port number
+	if len(sys.argv) == 3:
+		Port = int(sys.argv[2]) 
+	else:
+		#default port
+		Port = 8888
+
+	#Binds the server to an entered IP address and at the 
+	#specified port number. 
+	try:
+		server.bind((IP_address, Port)) 
+	except socket.error as err:
+		print err
+		exit()
+	server.listen(100) 
+	list_of_clients = [] 
+	clientNicknames = []
 
 
-while True: 
 
-	"""Accepts a connection request and stores two parameters, 
-	conn which is a socket object for that user, and addr 
-	which contains the IP address of the client that just 
-	connected"""
-	conn, addr = server.accept()
 
-	"""Maintains a list of clients for ease of broadcasting 
-	a message to all available people in the chatroom"""
-	list_of_clients.append(conn) 
-	conn.send("HELLO")
-	
 
-	"""while not uniqueName: # collect nickname from first message
-		message = conn.recv(2048)
-		if message:
-			sys.stdout.write(message)
-			nickname = message.rstrip() # strip endline
-			for names in clientNicknames:
-				print(clientNicknames)
-				if nickname != names:
-					conn.send("RETRY")
-					print("nickname already in use")
-					print ("try again...")
-					uniqueName = True
-					break
-				#if uniqueName:
-				#	clientNicknames.append(nickname)"""
-			
-	#message = conn.recv(2048)
-	#nickname = message.rstrip()
-	# prints the address of the user that just connected 
-	# creates and individual thread for every user 
-	# that connects 
-	start_new_thread(clientthread,(conn,addr, clientNicknames))	 
+	while True: 
 
-conn.close() 
-server.close() 
+		"""Accepts a connection request and stores two parameters, 
+		conn which is a socket object for that user, and addr 
+		which contains the IP address of the client that just 
+		connected"""
+		conn, addr = server.accept()
+
+		"""Maintains a list of clients for ease of broadcasting 
+		a message to all available people in the chatroom"""
+		list_of_clients.append(conn) 
+		conn.send("HELLO")
+		
+
+		"""while not uniqueName: # collect nickname from first message
+			message = conn.recv(2048)
+			if message:
+				sys.stdout.write(message)
+				nickname = message.rstrip() # strip endline
+				for names in clientNicknames:
+					print(clientNicknames)
+					if nickname != names:
+						conn.send("RETRY")
+						print("nickname already in use")
+						print ("try again...")
+						uniqueName = True
+						break
+					#if uniqueName:
+					#	clientNicknames.append(nickname)"""
+				
+		#message = conn.recv(2048)
+		#nickname = message.rstrip()
+		# prints the address of the user that just connected 
+		# creates and individual thread for every user 
+		# that connects 
+		start_new_thread(clientthread,(conn,addr, clientNicknames))	 
+
+	conn.close() 
+	server.close() 
+
+
+
+
+if __name__ == '__main__':
+    main()
