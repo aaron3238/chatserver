8d785e708fe2a558886630c9074819e380fb7a87
davidcnelson14 <davidcnelson14@gmail.com>
2020-11-22 23:03:08 -0500

    Update chatlib.py

---
+++ chatlib.py
@@ -0,0 +1,24 @@
+import socket 
+
+def read_msg(sock, maxbuff):
+
+	msg = sock.recv(maxbuff)
+	return msg
+	
+def write_msg(sock, msg, maxbuff):
+
+	# encode example found at https://stackoverflow.com/questions/30686701/python-get-size-of-string-in-bytes
+	length = len(msg.encode('utf-8'))
+	print(length)
+	if length <= maxbuff:
+		try:
+			sock.send(msg)
+			return 1
+		except: # can't send? 
+			sock.close()
+			#Need code to 
+			return 0
+	else:
+		print("Error: Message exceeds bufffersize")
+		return -1
+		
