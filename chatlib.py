4980810bbe16b75d047874c3adedd0d4e8c3f462
nmorello18 <73040163+nmorello18@users.noreply.github.com>
2020-11-24 12:15:05 -0500

    MORE DOCUMENTATION

--- chatlib.py
+++ chatlib.py
@@ -12,6 +12,10 @@
 import socket
 import sys 
 
+# Opens a new tcp socket as well as checking for errors
+# Name: socket_create
+# Arguments: None
+# Return Value: None
 def socket_create():
 
 	try:
@@ -20,6 +24,11 @@ def socket_create():
 	except socket.error as err:
 		sys.stdout.write("Error creating socket: %s" % err)
 
+# Reads a message from the socket
+# Name: read_msg
+# Arguments: sock - socket to read from
+#            maxbuff - max buffer size of the message to read
+# Return Value: None
 def read_msg(sock, maxbuff):
 
 	try:
@@ -27,7 +36,15 @@ def read_msg(sock, maxbuff):
 		return msg
 	except socket.error as err:
 		sys.stdout.write("Error reading data: %s" % err)
-	
+
+# write a message to a socket
+# Name: write_msg
+# Arguments: sock - socket to write to
+#            msg - message to send
+#            maxbuff - max buffer size of the message to send
+# Return Value: 1 - if message was successfully sent
+#              -1 - if there was an error
+#               0 - if the message exceeds the specified buffer size
 def write_msg(sock, msg, maxbuff):
 
 	# encode example found at https://stackoverflow.com/questions/30686701/python-get-size-of-string-in-bytes
@@ -38,8 +55,7 @@ def write_msg(sock, msg, maxbuff):
 			return 1
 		except socket.error as err: # can't send? 
 			sys.stdout.write("Error writing data: %s" % err)
-			sock.close()
-			#Need code to 
+			sock.close() # closes socket if there was an error
 			return -1
 	else:
 		print("Error: Message exceeds bufffersize")
